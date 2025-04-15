b624a8e01c5b98452a8d62e57fa79eb3f53817da
Devi Sri Charan <kasojudevisricharan@gmail.com>
2025-04-16 00:34:10 +0530

    url changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://blueflights-meta-search-engine-charans-projects-a67bea85.vercel.app/" }));
 app.use(express.json());
 
 // Routes
